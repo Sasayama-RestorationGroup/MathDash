4ef0367f7538d3e4525e41af8def0d754f235f8f
shureru <shureru.nifusu@gmail.com>
2019-10-31 22:07:16 +0900

    道生成クラス仮作成(左キーで左に曲がった、右キーで右に曲がったと仮定して道生成を行う)

---
+++ Assets/Scenes/Game/Scripts/QuestionRoad.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionRoad : MonoBehaviour
+{
+	/// <summary>
+	/// 分岐点
+	/// </summary>
+	[SerializeField]
+	private Transform curvePoint;
+
+	/// <summary>
+	/// 左の道のオブジェクト生成座標
+	/// </summary>
+	[SerializeField]
+	private Transform leftObjPoint;
+
+	/// <summary>
+	/// 右の道のオブジェクト生成座標
+	/// </summary>
+	[SerializeField]
+	private Transform rightObjPoint;
+
+    /// <summary>
+	/// 生成からの経過(問題回答回数)
+	/// </summary>
+	private int passTime;
+
+    void Awake()
+	{
+		passTime = 0;
+	}
+
+	public Transform GetCurveTransform()
+	{
+		return curvePoint.transform;
+	}
+
+	public Transform GetLeftTransform()
+	{
+		return leftObjPoint.transform;
+	}
+
+	public Transform GetRightTransform()
+	{
+		return rightObjPoint.transform;
+	}
+
+    /// <summary>
+	/// 1問回答するたびにコールされる。
+	/// </summary>
+    public void UpdateState()
+	{
+		passTime++;
+	}
+
+    /// <summary>
+	/// オブジェクト消すべきかどうか判定する
+	/// </summary>
+	/// <returns></returns>
+    public bool ShouldDestroy()
+	{
+        // 生成されてから3問回答されたら消す
+        if (passTime >= 3)
+		{
+			return true;
+		}
+		return false;
+	}
+}
--- Assets/Scenes/Game/Scripts/StageManager.cs
+++ Assets/Scenes/Game/Scripts/StageManager.cs
@@ -1,18 +1,119 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+	/// 現在走っている道
+	/// </summary>
+    [SerializeField]
+	private QuestionRoad currentRoad;
 
-    // Update is called once per frame
-    void Update()
+	/// <summary>
+	/// 次の左の道
+	/// </summary>
+	[SerializeField]
+	private QuestionRoad nextLeftRoad;
+
+	/// <summary>
+	/// 次の右の道
+	/// </summary>
+	[SerializeField]
+	private QuestionRoad nextRightRoad;
+
+    /// <summary>
+	/// 道のオブジェクトのプレハブ
+	/// </summary>
+	private GameObject roadObj;
+
+	private List<QuestionRoad> questionRoadList;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		// プレハブを取得
+		roadObj = (GameObject)Resources.Load("Prefabs/QuestionRoad");
+
+		questionRoadList = new List<QuestionRoad>();
+		questionRoadList.Add(currentRoad);
+		questionRoadList.Add(nextLeftRoad);
+		questionRoadList.Add(nextRightRoad);
+		currentRoad.UpdateState();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        // 左に曲がったとする
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			UpdateRoadList();
+			CreateNextRoad(nextLeftRoad);
+		}
+        // 右に曲がったとする
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			UpdateRoadList();
+			CreateNextRoad(nextRightRoad);
+		}
+	}
+
+    /// <summary>
+	/// 道の状態の更新
+	/// </summary>
+    private void UpdateRoadList()
+	{
+        // 道の状態更新
+        foreach(var road in questionRoadList)
+		{
+			road.UpdateState();
+		}
+
+        // 削除する道は削除用のリストに移してから削除する
+		var destroyList = questionRoadList.Where(road => road.ShouldDestroy()).ToList();
+		questionRoadList.RemoveAll(road => road.ShouldDestroy());
+
+        // 古い道は削除する
+        foreach(var road in destroyList)
+		{
+			Destroy(road.gameObject);
+		}
+	}
+
+	/// <summary>
+	/// 次の道の作成
+	/// </summary>
+	/// <param name="questionRoad">曲がった側のQuestionRoad</param>
+	private void CreateNextRoad(QuestionRoad _questionRoad)
+	{
+		// 次の道を作成する
+		var leftObjPoint = _questionRoad.GetLeftTransform();
+		var rightObjPoint = _questionRoad.GetRightTransform();
+		var leftObj = (GameObject)Instantiate(roadObj, leftObjPoint.position, leftObjPoint.rotation);
+		var rightObj = (GameObject)Instantiate(roadObj, rightObjPoint.position, rightObjPoint.rotation);
+		// 階層を"Stage"のオブジェクトの下にする
+		leftObj.transform.parent = this.transform;
+		rightObj.transform.parent = this.transform;
+
+		// リストに追加する
+		var leftScript = leftObj.GetComponent<QuestionRoad>();
+		var rightScript = rightObj.GetComponent<QuestionRoad>();
+		questionRoadList.Add(leftScript);
+		questionRoadList.Add(rightScript);
+
+        // オブジェクト名変更(for Debug)
+		currentRoad.gameObject.name = "OldCurrent";
+		nextLeftRoad.gameObject.name = "OldLeft";
+		nextRightRoad.gameObject.name = "OldRight";
+		_questionRoad.gameObject.name = "Current";
+		leftObj.name = "NextLeft";
+		rightObj.name = "NextRight";
+
+		// 現在の道の状態を更新する。
+		currentRoad = _questionRoad;
+		nextLeftRoad = leftScript;
+		nextRightRoad = rightScript;
+	}
 }
